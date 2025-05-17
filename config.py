686b4e7238b31691cf4c2a7fbc6941212930f970
danilo631 <defreitasfernandesdanilo81@gmail.com>
2025-05-17 15:28:52 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 TELEGRAM_TOKEN = '7586808863:AAEM92xbhc8TP8VSGz8QdTBaJOu3jwhuiNA'
-OPENROUTER_API_KEY = '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'
-IA_MODEL = 'qwen/qwen3-4b:free'
+OPENROUTER_API_KEY = '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'
+IA_MODEL = 'meta-llama/llama-4-maverick:free'
 DB_FILE = 'mensagens.db'
