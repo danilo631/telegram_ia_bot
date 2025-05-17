839b37a9f9046df641625a2ef314b5e85de4f8e9
danilo631 <defreitasfernandesdanilo81@gmail.com>
2025-05-17 14:10:58 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 TELEGRAM_TOKEN = '8141251243:AAE1oYBwcv8baKJsQkvdOE9DdZUipTwjQC8'
 OPENROUTER_API_KEY = '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'
-IA_MODEL = 'openchat/openchat-3.5'
+IA_MODEL = 'meta-llama/llama-3.3-8b-instruct:free'
 DB_FILE = 'mensagens.db'
